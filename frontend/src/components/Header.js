110b85346f4eb081e0ad524088e030b885a6389b
François Costa <frans1998@gmail.com>
2024-01-02 21:31:15 +0100

    analytics page

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 import '../css/Header.css';
 
 export default function Header() {
 
-    return (
-        <header className="top-header">
-          <div className="title-section">
-            <Link to="/" className="title-link">
-              What's your YouTube count?
-            </Link>
-          </div>
-          <div className="right-section">
-            <Link to="/analytics" className="nav-link">
-              Analytics
-            </Link>
-            <Link to="/about" className="nav-link">
-              About us
-            </Link>
-          </div>
-        </header>
-      );
+  return (
+    <header className="top-header">
+      <div className="title-section">
+        <Link to="/" className="title-link">
+          What's your YouTube count?
+        </Link>
+      </div>
+      <div className="right-section">
+        <NavLink to="/analytics" className={({ isActive, isPending }) =>
+          isPending ? "nav-link" : isActive ? "nav-link active-link" : "nav-link"
+        }>
+          Analytics
+        </NavLink>
+        <NavLink to="/about" className={({ isActive, isPending }) =>
+          isPending ? "nav-link" : isActive ? "nav-link active-link" : "nav-link"
+        }>
+          About us
+        </NavLink>
+      </div>
+    </header>
+  );
 }
